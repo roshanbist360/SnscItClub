c59102adb39809a1b945e55fe0137d08199c5ce5
Roshan Bist <roshanbist900@gmail.com>
2022-06-12 07:20:09 -0700

    first

---
+++ index.js
@@ -0,0 +1,84 @@
+        
+
+    // js dom for opening and closing side nav in mobile
+    function openNav(){
+          document.getElementById('sideNavv').style.width="320px";
+     }
+
+    function closeNav(){
+         document.getElementById('sideNavv').style.width="0px";
+    }    
+
+
+    // jquery     
+    $(document).ready(function(){
+        // get scroll value so that various changes can be done on scroll
+        // for to appear navbar on scroll
+         window.addEventListener("scroll",function (event){
+             var scroll=scrollY;
+         });
+
+        //Below js is for well working of go to top button
+        $(window).scroll(function(){
+           if($(this).scrollTop()>500){
+               $('#topButton').fadeIn();
+           }
+           else{
+               $('#topButton').fadeOut(); 
+           }
+         });
+
+           $("#topButton").click(function(){
+           $('html','body').animate({scrollTop:0},400);
+        });
+
+        // Testimonials carousel (uses the Owl Carousel library)
+         $('.owl-carousel').owlCarousel({
+         loop:true,
+         margin:10,
+         nav:false,
+         dots:true,
+         autoplay:true,
+         autoplaySpeed: 1000,
+         autoplayTimeout: 8000,
+         number:1,
+         responsive:{
+         0:{
+         items:1
+         },
+         600:{
+         items:1
+         },
+         1000:{
+         items:2
+         }
+         }
+         })
+
+         // <!-- for making text title auto typing -->
+          var typed = new Typed('.type', {
+          strings: ['ELCOME TO SNSC IT CLUB'],
+          typeSpeed: 100,
+          backSpeed:100,
+          loop:true,
+          backDelay: 1500,
+          showCursor: false,
+          });
+
+    });
+
+
+        
+          
+        
+
+
+
+
+
+
+
+
+
+
+
